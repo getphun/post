f036df4dec87b6d270be42fc6a22daf8d52599c3
Iqbal Fauzi <iqbalfawz@gmail.com>
2017-09-15 14:43:46 +0700

    Add iframe on content fixer

--- theme/site/static/js/post.js
+++ theme/site/static/js/post.js
@@ -2,7 +2,7 @@ $(function(){
     
     $('[data-post="content"]').each(function(i,e){
         var el = $(e);
-        var width = el.innerWidth();
+        var elWidth = el.innerWidth();
         
         // fix images
         el.find('img').each(function(i,img){
@@ -15,9 +15,41 @@ $(function(){
                 return;
             
             // resize it to fit the content
-            newsize = '_' + width + 'x';
+            newsize = '_' + elWidth + 'x';
             src = src.replace(/\.([a-zA-Z]+)$/, newsize + '\.$1');
             $img.attr('src', src);
+        });
+        
+        // fix iframes
+        el.find('iframe').each(function(i,ifr){
+            var $ifr  = $(ifr);
+            if($ifr.hasClass('leave-me-alone'))
+                return;
+            
+            var iwidth  = parseInt($ifr.attr('width'));
+            var iheight = parseInt($ifr.attr('height'));
+            var isrc    = $ifr.attr('src');
+            
+            $ifr.removeAttr('width');
+            $ifr.removeAttr('height');
+            
+            var parent = $('<div></div>');
+            
+            if(/youtube|youtu\.be/.test(isrc)){
+                parent.addClass('embed-responsive embed-responsive-16by9');
+            }else{
+                if(!iwidth || !iheight){
+                    parent.addClass('embed-responsive embed-responsive-16by9');
+                }else{
+                    $ifr.css({width:'100%',height:'100%',position:'absolute',left:0,top:0});
+                    parent.css({position:'relative',width:'100%'});
+                    var percent = ( iheight / iwidth ) * 100;
+                    parent.css('padding-bottom', percent + '%');
+                }
+            }
+            
+            $ifr.before(parent);
+            parent.append($ifr);
         });
     });
 });
