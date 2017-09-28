3e4fb14039b198fbdcf6ced92138796eb4c55f5d
Iqbal Fauzi <iqbalfawz@gmail.com>
2017-09-28 16:47:30 +0700

    Fix minor bug on image resizer

--- theme/site/static/js/post.js
+++ theme/site/static/js/post.js
@@ -83,14 +83,14 @@ window.XPost = {
         img.addClass('img-responsive');
         
         // can we resize them?
-        var src = $img.attr('src');
+        var src = img.attr('src');
         if(src.substr(0,4) == 'http')
             return;
         
         // resize it to fit the content
         newsize = '_' + maxw + 'x';
         src = src.replace(/\.([a-zA-Z]+)$/, newsize + '\.$1');
-        $img.attr('src', src);
+        img.attr('src', src);
     },
     
     init: function(){
